dbe209331d8dea1947ad7be2acc956937d4c46bb
Marc Steen Fordsmand <marc5517@edu.easj.dk>
2020-12-07 11:03:23 +0100

    Lavet en GetById metode

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,21 +1,39 @@
+import axios, {
+    AxiosResponse,
+    AxiosError
+} from "../../node_modules/axios/index"
+
+interface ICoronaTest {
+    testId: number
+    machineName: string
+    temperature: number
+    location: string
+    date: string
+    time: string
+}
+
+let baseUrl: string = "https://coronatest.azurewebsites.net/api/CoronaTests"
+
 new Vue({
     // TypeScript compiler complains about Vue because the CDN link to Vue is in the html file.
     // Before the application runs this TypeScript file will be compiled into bundle.js
     // which is included at the bottom of the html file.
     el: "#app",
     data: {
-        name: "",
-        greeting: ""
+        idToGetBy: -1,
+        singleTest: null
     },
     methods: {
-        sayHello() {
-            console.log("Say Hello " + this.name)
-            if (this.name == "") {
-                this.greeting = "Hello NoName"
-            }
-            else {
-                this.greeting = "Hello " + this.name
-            }
+        getById(id: number) {
+            let url: string = baseUrl + "/" + id
+            axios.get<ICoronaTest>(url)
+                .then((response: AxiosResponse<ICoronaTest>) => {
+                    this.singleTest = response.data
+                })
+                .catch((error: AxiosError) => {
+                    //this.message = error.message
+                    alert(error.message) // https://www.w3schools.com/js/js_popup.asp
+                })
         }
     }
 })
